193a49972ca35588504bff933471bf9d26f8eeac
Mohammed Sisay <155180503+Mame123sisay@users.noreply.github.com>
2026-01-03 13:59:28 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,12 +9,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 // Allow only your frontend origin 
 app.use(cors({
-   origin: "https://todo-app-727u.onrender.com", 
+   origin: FRONTEND_ORIGIN, 
    methods: ["GET", "POST", "PUT", "DELETE"],
    credentials: true }));
 app.use(express.json());
 app.use('/api/todo',router)
-app.get("/health", (req, res) => { res.status(200).send("OK"); });
+
 
 
 
